fe6ca75ecb2bc4de084e743d5560b395ef1cfbe3
Leonardo Barbosa <leobarbosajf@hotmail.com>
2025-08-24 17:58:55 -0300

    Atualização do título

--- aula005_arrays/entrada_dados_listando_itens.php
+++ aula005_arrays/entrada_dados_listando_itens.php
@@ -35,7 +35,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST' && !isset($_POST['limpar'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastrp de Alunos (com Session)</title>
+    <title>Cadastro de Alunos (com Session)</title>
     <style>
     body {
         font-family: Arial;
