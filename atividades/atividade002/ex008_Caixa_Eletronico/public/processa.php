cd0130079e07e69fd89a24e445d51d2111ac9175
Leonardo Barbosa <leobarbosajf@hotmail.com>
2025-08-25 15:14:55 -0300

    Funcionando nota de 100

--- atividades/atividade002/ex008_Caixa_Eletronico/public/processa.php
+++ atividades/atividade002/ex008_Caixa_Eletronico/public/processa.php
@@ -1,10 +1,10 @@
 <?php
     function sacar($valor){
         $resto = 0;
-        if($valor > 100){
+        if($valor >= 100){
             $resto = $valor % 100;
             $numeroCelulas = intdiv($valor, 100);
-            
+            echo "Será liberado $numeroCelulas cédula(s) de R$100,00 , $resto";
         }
     }
 ?>
